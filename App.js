796998d7ed43ae30d1eb4c3ddef55132d73794d8
alicegardell <alice.gardell.6137@student.uu.se>
2022-04-04 16:25:30 +0200

    setup database

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import FetchListScreen from './components/fetchlist_screen.js'
+
+
 
 export default function App() {
   return (
+    <View style = {styles.container}> 
+    <FetchListScreen/>
     <NavigationContainer><View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
     </NavigationContainer>
+    </View>
     
-  );
+      );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ config/firebase.js
@@ -0,0 +1,18 @@
+import  firebase  from 'firebase/compat';
+import firestore from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvI83PTRVappg6O9-c5cTXyPv9qy5bX50",
+    authDomain: "sporta-47292.firebaseapp.com",
+    projectId: "sporta-47292",
+    storageBucket: "sporta-47292.appspot.com",
+    messagingSenderId: "524060733251",
+    appId: "1:524060733251:web:c2e17c6d417ba85ccd09f6",
+    measurementId: "G-S75K31GBVC"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.firestore();
+
+export default firebase;
