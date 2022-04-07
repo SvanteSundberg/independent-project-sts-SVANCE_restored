f6b00e8d18fac066c394987704a78920255883db
vilmahagg <vilmahggedelnn@gmail.com>
2022-04-07 14:57:45 +0200

    modal screen and datepicker in createEvent

--- AppNavigator.jsx
+++ AppNavigator.jsx
@@ -2,12 +2,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import  StartScreen from './screens/StartScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
+import CreateEventScreen from "./screens/CreateEventScreen.jsx";
+import TimelineScreen from "./screens/TimelineScreen.jsx";
 
 const {Navigator, Screen}= createNativeStackNavigator();
 
 const AppNavigator = () => (
     <NavigationContainer>
         <Navigator  initialRouteName="StartScreen" >
+        
             <Screen name="StartScreen" 
                     component={StartScreen} 
                     options={{headerShown: false}}>
@@ -17,6 +20,14 @@ const AppNavigator = () => (
                     component={LoginScreen} 
                     options={{
                     title:"Sign in", }}>
+            </Screen>
+
+            <Screen name="TimelineScreen"
+                component={TimelineScreen}>
+            </Screen>
+
+            <Screen name="CreateEventScreen"
+                component={CreateEventScreen}>
             </Screen>
         </Navigator>
     </NavigationContainer>
--- screens/CreateEventScreen.jsx
+++ screens/CreateEventScreen.jsx
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View, ScrollView, KeyboardAvoidingView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Modal, Image, TouchableHighlight, Pressable, KeyboardAvoidingView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useForm } from "react-hook-form";
-import { TextInput, Button, Modal, Portal, Provider } from "react-native-paper";
+import { TextInput, Button, Portal, Provider } from "react-native-paper";
 import React from 'react';
 import colors from '../config/colors.js';
 import firebase from '../config/firebase';
 import { setStatusBarNetworkActivityIndicatorVisible } from "expo-status-bar";
-
+import { DatePicker } from "react-native-common-date-picker";
+// import { useNavigation } from '@react-navigation/native'; //uncomment to use navigation
 
 function CreateEventScreen(props) {
 
@@ -16,16 +17,23 @@ function CreateEventScreen(props) {
     const [date, setDate] = React.useState("");
     const [noPeople, setNoPeople] = React.useState("");
 
-    const pressSend = () => {
-       /*  sendEvent(); */
-        /* showModal(); */
-    }
+    // const navigation= useNavigation(); //uncomment to use navigation
 
-    /* const [visible, setVisible] = React.useState(false);
-    const showModal = () => setVisible(true);
-    const hideModal = () => setVisible(false);
-    const containerStyle = { backgroundColor: 'white', padding: 20, height: 300 }
- */
+    const pressSend = () => {
+        checkInput();
+    }
+    const [isVisible, setVisible] = React.useState(false);
+    const checkInput = () => {
+        if (!title.trim() || !description.trim() || !place.trim() || !date.trim() || !noPeople.trim()) {
+            alert('Please fill out all fields');
+            return;
+        }
+        //  sendEvent(); //uncomment to send to database
+        toggleModal();
+    };
+    const toggleModal = () => {
+        setVisible(!isVisible);
+    };
     const sendEvent = () => firebase.firestore().collection('events').add({
         title: title,
         description: description,
@@ -33,15 +41,16 @@ function CreateEventScreen(props) {
         date: date,
         noPeople: noPeople,
         createdAt: firebase.database.ServerValue.TIMESTAMP
-    })
+    });
 
 
 
 
     return (
         <SafeAreaView style={styles.main}>
-            <ScrollView style={styles.ScrollView}>
+            <ScrollView style={styles.ScrollView} nestedScrollEnabled={true}>
                 <Text style={styles.header}> CREATE EVENT </Text>
+
 
 
                 <View style={styles.form}>
@@ -91,10 +100,49 @@ function CreateEventScreen(props) {
 
                     />
 
+                    <DatePicker
+                        backgroundColor="white"
+
+                        minDate='2022-04-01'
+                        maxDate='2022-07-01'
+                        monthDisplayMode={'en-short'}
+                        titleText="Choose a date for the event"
+                        cancelText=""
+                        rows={5}
+                        selectedRowBackgroundColor="#C2E1C2"
+                        width={350}
+
+                        confirm={(date) => {
+                            console.log(date);
+                        }}
+                    />
+
                     <Button style={[styles.button, styles.createEventButton]} title="send" mode="contained" onPress={pressSend}>
                         Create Event
                     </Button>
 
+                    <Modal
+                        animationType={"fade"}
+                        visible={isVisible}
+                        transparent={true}
+                    >
+                        <View style={styles.centerView}>
+                            <View style={styles.modalView}>
+                                <Image source={require('../assets/green-checkmark.png')} style={styles.modalLogo} />
+                                <Text style={styles.modalText}>You successfully created an event!</Text>
+
+                                <Button
+                                    style={[styles.button, styles.modalButton]}
+                                    onPress={() => toggleModal()}// navigation.navigate("TimelineScreen")} //comment/uncomment for navigation
+                                >
+                                    <Text style={styles.buttonText}>
+                                        Back To Timeline</Text>
+                                </Button>
+
+                            </View>
+                        </View>
+
+                    </Modal>
                 </View>
 
             </ScrollView>
@@ -104,6 +152,7 @@ function CreateEventScreen(props) {
 
 const styles = StyleSheet.create({
     main: {
+        flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
     },
@@ -111,13 +160,25 @@ const styles = StyleSheet.create({
         fontSize: 20,
         backgroundColor: colors.backgroundColor,
     },
+    ScrollView: {
+        flex: 1,
+        width: '100%',
+    },
     form: {
+        flex: 1,
         padding: 10,
 
+    },
+    buttonText: {
+        color: 'white',
     },
     button: {
         width: 300,
         margin: 10,
+    },
+    modalButton: {
+        backgroundColor: colors.accentColor,
+        shadowColor: 'white',
     },
     createEventButton: {
         backgroundColor: colors.accentColor,
@@ -125,6 +186,35 @@ const styles = StyleSheet.create({
     },
     input: {
 
+    },
+    centerView: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    modalLogo: {
+        height: 60,
+        width: 60,
+    },
+    modalText: {
+        color: 'black',
     }
 });
 
