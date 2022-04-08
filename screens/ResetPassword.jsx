b5c9e934cdb2cbecc46a472111e0fae01c3f96e7
Svante <svante.lars.sundberg@gmail.com>
2022-04-08 13:37:21 +0200

    Styling for ResetPassword

--- screens/ResetPassword.jsx
+++ screens/ResetPassword.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { StyleSheet, ActivityIndicator, View, Text, Alert } from 'react-native';
-import { Button, Input , Icon, TextInput} from 'react-native-paper';
+import { StyleSheet, View, Text, Alert } from 'react-native';
+import { Button, TextInput} from 'react-native-paper';
 import { auth } from "../config/firebase";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const ResetPassword = () => {
     
@@ -9,7 +10,6 @@ const ResetPassword = () => {
     const [email, setEmail] = useState('');
 
     const reset = async() => {
-         
         try {
             await auth.sendPasswordResetEmail(email);
             console.log('email sent');
@@ -23,31 +23,31 @@ const ResetPassword = () => {
         }
     };
 
-    const renderBtn =() =>{
-        if( isSent){
+    const renderView =() =>{
+        if(isSent){
             return(
-            <View>
-                <Text> hej</Text>
+            <View style={styles.emailSent}>
+                <Ionicons name="md-checkmark-circle" size={50} color="green" />
+                <Text>Link to reset your password sent. Please check your inbox.</Text>
             </View>)
         }
         else {
             return(
             <View style={styles.container}>
-            <Text style={{ fontSize: 28, height: 50}}>Reset Password!</Text>
+            <Text style={{ fontSize: 28, height: 50, alignSelf:'center'}}>Reset your password</Text>
             <TextInput
                     style={styles.textInput}
                     placeholder='Your Email'
                     value={email}
                     onChangeText={setEmail}
                     mode="outlined"
-                    activeOutlineColor="hotpink"
-                
-                />
-                        <Button
-                    style={styles.textInput}
+                    activeOutlineColor="hotpink"/>
+                    <Button
+                    style={styles.btn}
                     mode="contained"
-                    onPress={() => reset()}> Reset</Button>  
-        
+                    onPress={() => reset()}> 
+                    Reset
+                    </Button>     
         </View>
             )
 
@@ -57,11 +57,9 @@ const ResetPassword = () => {
 
     return (
         <React.Fragment>
-         
-        
-        {renderBtn()}
-        
 
+        {renderView()}
+        
         </React.Fragment>
 
     );
@@ -70,11 +68,19 @@ const ResetPassword = () => {
   const styles = StyleSheet.create({
     container: {
         flex: 1,
-        
         justifyContent:'center'
     },
     textInput:{
         margin:20
+    },
+    btn:{
+        width:200,
+        alignSelf:'center'
+    },
+    emailSent:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems:'center'
     }
     
 })
