b3130f2092170004f57840bd5f7557545c769623
Nils Risberg <nilsrisberg3@gmail.com>
2022-04-11 11:34:42 +0200

    timeline

--- screens/TimelineScreen.jsx
+++ screens/TimelineScreen.jsx
@@ -1,13 +1,17 @@
-import { StyleSheet,View, Text, TextInput, SafeAreaView, Button, TouchableOpacity } from "react-native";
+import { StyleSheet,View, Text, TextInput, SafeAreaView, Button, TouchableOpacity, ScrollView } from "react-native";
 import * as React from 'react';
 import { Checkbox } from 'react-native-paper';
+import { IconButton, Colors } from 'react-native-paper';
  
  
  
 const Timeline = () => {
     const array1 = {Nils:"jag vill spela fotboll",
                     Svante: "jag vill spela volyboll",
-                    Carro: "jag vill springa"};
+                    Carro: "jag vill springa",
+                    Vilma:"jag vill spela fotboll",
+                    Alice: "jag vill spela volyboll",
+                    Emelie: "jag vill springa"};
     const results = [];
        
    
@@ -17,31 +21,74 @@ for (let element of Object.entries(array1)) {
     results.push(
     <View style ={styles.allposts}>
       <SafeAreaView>
+         
             {bigpost?(
             <TouchableOpacity style = {styles.posts} onPress={() => {
                 setbigpost(!bigpost);
               }}>
                 <Text>{element[0]}</Text>
                 <Text>{element[1]}</Text>
-                <Checkbox
+               <View style = {styles.joinbutton}><Checkbox
       status={checked ? 'checked' : 'unchecked'}
+      color="black"
+      uncheckedColor="black"
       onPress={() => {
         setChecked(!checked);
       }}
-    />
+               
+    /></View>
             </TouchableOpacity>):null}
            
            
             {!bigpost?(<TouchableOpacity style = {styles.smallposts} onPress={() => {
         setbigpost(!bigpost);
       }}>
+          <Text>{element[0]}</Text>
             </TouchableOpacity>):null}
  
         </SafeAreaView>
     </View>
   );
 }
-    return ( results );
+ 
+/*results.push(
+    <SafeAreaView style ={styles.createEvent}>
+     
+      <IconButton
+    icon="pencil-circle-outline"
+    color={Colors.black}
+    size={40}
+    onPress={() => console.log('Pressed')}
+  />
+   
+    </SafeAreaView>
+   
+); */
+ 
+    return (<View>
+        <SafeAreaView><Text style={styles.header}>Aktiviteter</Text></SafeAreaView>
+        <ScrollView>{results}</ScrollView>
+    <SafeAreaView style ={styles.createEvent}>
+     
+    <IconButton
+  icon="pencil-circle"
+  color={Colors.black}
+  size={40}
+  onPress={() => console.log('Pressed')}
+/>
+ 
+  </SafeAreaView>
+  <SafeAreaView style ={styles.profile}>
+     
+    <IconButton
+  icon="account-circle"
+  color={Colors.black}
+  size={40}
+  onPress={() => console.log('Pressed')}
+/>
+ 
+  </SafeAreaView>
+  </View>);
     }
  
  
@@ -51,12 +98,28 @@ for (let element of Object.entries(array1)) {
 const styles = StyleSheet.create({
     header:{
         fontSize:30,
-        flex: 1,
-        maxHeight:50,    
-    },
-    joinbutton:{
+        maxHeight:50,
+        marginTop:15,
+        marginLeft:130,
+        alignItems: 'center',
        
     },
+    joinbutton:{
+        borderWidth: 2,
+            borderRadius: 10,
+            borderColor:"black",
+    },
+    createEvent:{
+        marginLeft:320,
+        marginTop:700,
+        position: 'absolute',
+    },
+    profile:{
+        marginLeft:5,
+        marginTop:0,
+        position: 'absolute',
+    },
+   
     allposts:{
         alignItems: 'center',
     justifyContent: 'center',
@@ -64,7 +127,7 @@ const styles = StyleSheet.create({
     smallposts:{
         alignItems: 'center',
         justifyContent: 'center',
-            width:300,
+            width:250,
             height:100,
             marginTop:5,
             padding:5,
@@ -75,7 +138,7 @@ const styles = StyleSheet.create({
     posts:{
         alignItems: 'center',
     justifyContent: 'center',
-        width:300,
+        width:250,
         height:200,
         marginTop:5,
         padding:5,
@@ -86,3 +149,6 @@ const styles = StyleSheet.create({
 });
  
 export default Timeline;
+ 
+
+
