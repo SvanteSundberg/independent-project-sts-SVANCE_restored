3d9f290f7632900c05a9529ec96e3eb15002924c
emelieasbrink <emelie.asbrink@gmail.com>
2022-04-21 15:55:43 +0200

    Merge conflict fixed

--- screens/TimelineScreen.jsx
+++ screens/TimelineScreen.jsx
@@ -118,20 +118,11 @@ let uniqueObjArray = [
 />
  
   </SafeAreaView>
-  <SafeAreaView style ={styles.profile}>
-     
-    <IconButton
-  icon="account-circle"
-  color={Colors.black}
-  size={40}
-  onPress={() => navigation.navigate("ProfileScreen", {
-    userID: user.uid
-  })}
-/>
- 
   </SafeAreaView>
+
+
     );
-    }
+  }
  
  
  
